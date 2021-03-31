3c85072cc46fb25a5359a3d529b8135b7965b579
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-03-31 22:10:42 +0600

    connection setup to the database

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5055
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wgngx.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+client.connect(err => {
+  const collection = client.db(`${process.env.DB_DATABASE}`).collection(`${process.env.DB_COLLECTION}`);
+    // perform actions on the collection object
+
+    //   client.close();
+});
+
+
+
+
+app.listen(port)
